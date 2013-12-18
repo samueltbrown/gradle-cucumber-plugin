663ed5d2324c1a00de0cac4592fc324da5fb14dc
Philippe Vosges <philippe.vosges@gmail.com>
2013-12-17 22:52:44 -0800

    added Gradle plugin manifest

---
+++ src/test/groovy/com/excella/gradle/cucumber/CucumberPluginTest.groovy
@@ -0,0 +1,17 @@
+package com.excella.gradle.cucumber
+
+import com.excella.gradle.cucumber.tasks.CucumberTask
+import static org.junit.Assert.*
+import org.gradle.api.Project
+import org.gradle.testfixtures.ProjectBuilder
+import org.junit.Test
+
+class CucumberPluginTest {
+
+    @Test
+    public void cucumberTaskAddedToProject() {
+        Project project = ProjectBuilder.builder().build()
+        project.apply plugin: 'cucumber'
+        assertTrue(project.tasks.cucumber instanceof CucumberTask)
+    }
+}
